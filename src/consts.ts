868bb9b3c039aa020952d56a473e41ebd6a32ea2
henriquepacheco27 <henriquepacheco27@gmail.com>
2025-02-24 17:17:04 -0300

    blog 1

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
+export const SITE_URL = "https://threedolar.com.br/";
 export const SITE_TITLE = "hp27books";
 export const SITE_DESCRIPTION =
   "Minimal blog, big knowledge";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "henriquepacheco27";
+
